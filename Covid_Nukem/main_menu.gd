25a102fcbd93194da0ffdc9fe42f6aa064d1491a
matekagan <matekagan@gmail.com>
2020-04-17 18:26:01 +0200

    Change player Input from Keyboard to Mouse

--- Covid_Nukem/main_menu.gd
+++ Covid_Nukem/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 var level = preload("res://game.tscn")
 
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	$HBox/MainContainer/VBox/Exit.connect("button_down", self ,"_exit")
 	$HBox/MainContainer/VBox/Start.connect("button_down", self, "_start")
 
--- Covid_Nukem/player.gd
+++ Covid_Nukem/player.gd
@@ -12,33 +12,15 @@ func _ready():
 		get_viewport_rect().size.x * 0.5,
 		get_viewport_rect().size.y * 0.5
 	)
-	
-func _process(delta):
-	if handle_input():
-		velocity -= velocity * 20.0 * delta
-	velocity.x = min( abs(velocity.x), max_speed ) * sign( velocity.x )
-	velocity.y = min( abs(velocity.y), max_speed ) * sign( velocity.y )
-	position += velocity * delta
-	position.y = clamp(position.y, 0, get_viewport_rect().size.y)
-	position.x = clamp(position.x, 0, get_viewport_rect().size.x)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
-func handle_input():
-	var key_pressed = false
-	if Input.is_action_pressed("ui_cancel"):
+func _input(event):
+	if event is InputEventKey && event.get_scancode() == KEY_ESCAPE:
 		exit()
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= speed
-		key_pressed = true
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += speed
-		key_pressed = true
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= speed
-		key_pressed = true
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += speed
-		key_pressed = true
-	return !key_pressed
-	
+		return
+	if event is InputEventMouseMotion:
+		position = event.position
+
 func exit():
-	get_tree().quit()
+	var menu = load('res://main_menu.tscn')
+	get_tree().change_scene_to(menu)
